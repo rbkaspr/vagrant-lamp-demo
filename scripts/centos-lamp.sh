50392c5a5a2011e035f4c450aeb83aca5c41345a
Ryan Kasprzyk <ryan.b.kasprzyk@gmail.com>
2020-04-13 11:55:30 -0400

    Added community mysql repo

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -20,6 +20,7 @@ systemctl start httpd
 yum install -y php php-cli php-common php-mysql
 
 # Install mysql
+yum install -y https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 yum install -y mysql mysql-server mysql-devel
 systemctl enable mysql
 systemctl start mysqld
