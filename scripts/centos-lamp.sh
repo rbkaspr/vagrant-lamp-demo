a494cddc5425dbb09138cfa0adaafb21c48c5caa
Ryan Kasprzyk <ryan.b.kasprzyk@gmail.com>
2020-04-13 14:20:12 -0400

    Removed default security for mysql

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -22,6 +22,12 @@ yum install -y mysql mysql-server mysql-devel
 systemctl enable mysqld
 systemctl start mysqld
 
+# Cleanup root password
+password=$(grep -oP 'temporary password(.*): \K(\S+)' /var/log/mysqld.log)
+mysqladmin --user=root --password="$password" password aaBB@@cc1122
+mysql --user=root --password=aaBB@@cc1122 -e "UNINSTALL COMPONENT 'file://component_validate_password';"
+mysqladmin --user=root --password="aaBB@@cc1122" password ""
+
 mysql -u root -e "SHOW DATABASES";
 
 # Install PHP
