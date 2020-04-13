ef3853c51263d562d54a35e29caeab022310b9f7
Ryan Kasprzyk <ryan.b.kasprzyk@gmail.com>
2020-04-13 11:16:11 -0400

    Added LAMP files

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update OS
+yum update -y --exclude-kernel
+
+# Add dev utils
+yum install -y vim unzip git screen
+
+# Install Apache
+yum install -y httpd httpd-devel httpd-tools
+systemctl enable httpd
+systemctl start httpd
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+systemctl start httpd
+
+# Install PHP
+yum install -y php php-cli php-common php-mysql
+
+# Install mysql
+yum install -y mysql mysql-server mysql-devel
+systemctl enable mysql
+systemctl start mysqld
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download starter content
+
+systemctl restart httpd
